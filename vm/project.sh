a63a99f7a8fa7aa27155e6ab538bd953e5941da1
Justin Gonz <justinb2@unm.edu>
2022-07-29 14:45:30 -0600

    final

--- vm/project.sh
+++ vm/project.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-exec qemu-system-arm -M versatilepb -kernel zImage -dtb versatile-pb.dtb -drive file=rootfs.ext2,if=scsi,format=raw -append "rootwait root=/dev/sda console=ttyAMA0,115200"  -net nic,model=rtl8139 -net user,hostfwd=tcp::2222-:22
+# had to do this before running to get to work?
+# export PATH="/home/justinb/git/buildroot/output/host/bin:${PATH}"
+qemu-system-arm -M versatilepb -kernel zImage -dtb versatile-pb.dtb -drive file=rootfs.ext2,if=scsi,format=raw -append "root=/dev/sda console=ttyAMA0,115200" -serial stdio -net nic,model=rtl8139 -net user,hostfwd=tcp::2222-:22 -name Versatile_ARM_EXT2
